4bea10615a059a1358ae75f546d15b750dd4af30
kishan_v_nobroker <kishan.vishwakarma@nobroker.in>
2025-09-21 01:35:19 +0530

    Update package dependencies to include @gsap/react and ogl; add DarkVeil component to App and change background color to black in index.css

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import TextTrail from './elements/Text/textTrail/TextTrail'
+import DarkVeil from './elements/Background/DarkVeil/DarkVeil'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <div style={{ width: '100%', height: '600px', position: 'relative' }}>
+        <DarkVeil />
+      </div>
     </>
   )
 }
